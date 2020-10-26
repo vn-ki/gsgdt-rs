87975f645bc7df78a56b93680cd346c34c0e1d9d
Vishnunarayan K I <appukuttancr@gmail.com>
2020-10-26 18:40:24 +0530

    chore: add readme and cargo.toml metadata

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,29 @@ mod tests {
     use crate::*;
     #[test]
     fn it_works() {
-        let stmts: Vec<String> = vec!["hi".into(), "hell".into()];
         let label1: String = "bb0__0_3".into();
-        let style: NodeStyle = Default::default();
-        let node1 = Node::new(stmts, label1.clone(), "0".into(), style.clone());
-
-        let stmts: Vec<String> = vec!["_1 = const 1_i32".into(), "_2 = const 2_i32".into()];
         let label2: String = "bb0__1_3".into();
-        let node2 = Node::new(stmts, label2.clone(), "1".into(), style.clone());
+        let style: NodeStyle = Default::default();
+
+        let nodes = vec![
+            Node::new(
+                vec!["_1 = const 1_i32".into(), "_2 = const 2_i32".into()],
+                label1.clone(),
+                "0".into(),
+                style.clone(),
+            ),
+            Node::new(
+                vec!["return".into()],
+                label2.clone(),
+                "1".into(),
+                style.clone(),
+            ),
+        ];
 
         let g = Graph::new(
             "Mir_0_3".into(),
             GraphKind::Digraph,
-            vec![node1, node2],
+            nodes,
             vec![Edge::new(label1, label2, "return".into())],
         );
 
